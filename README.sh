8da0f7ff35f47ca58b3f02bde08893c48975e71e
Vasiliy Kevroletin <kevroletin@gmail.com>
2016-09-24 08:19:51 +0000

    Add README with bootstrap commands for Ubuntu

---
+++ README.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Usefull commands:
+#   vcsh list
+#
+# Examine .config/mr/available.d and .config/mr/config.d
+
+pushd $HOME; sudo apt-get update
+
+if ! sh -c 'which git 2>/dev/null'; then sudo apt-get install git; done
+git config --global user.name "Vasiliy Kevroletin"
+git config --global user.email kevroletin@gmail.com
+
+sudo apt-get install vcsh mr emacs24-nox zsh ranger
+sudo chsh `whoami` `which zsh`
+
+vcsh clone https://github.com/vkevroletin/dotfiles.mr
+mr checkout
