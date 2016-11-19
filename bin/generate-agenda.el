ce5443ac3b91406a404c5bdc6f3e5680c2d02081
Vasiliy Kevroletin <kevroletin@gmail.com>
2016-11-19 16:30:53 +1000

    Generate and send html agenda using emacs
    There are several problems which caused me to duplicate some of elisp code:
    1. emacs htmlize package doesn't use theme styles in batch mode
       (see https://stackoverflow.com/questions/3591337/emacs-htmlize-in-batch-mode)
    2. loading only part of my condig didn't work because of problem similar to
       https://github.com/syl20bnr/spacemacs/issues/2717
    
    So current solution is quite simple:
    1. Write minimal emacs list script to configure org-mode to generate right agenda. It includes
       customization from my emacs config but I don't expect it to change often.
    2. Don't load whole config, just run minimal script from step 1 to generate html agenda.
    3. Don't use emacs theme. Just tweak backgroun and foreground colors.

---
+++ bin/generate-agenda.el
@@ -0,0 +1,51 @@
+(package-initialize)
+
+(require 'org)
+(require 'org-agenda)
+
+(add-to-list 'org-modules 'org-habit)
+
+(setq org-agenda-files (list "~/org/personal"))
+
+(setq org-capture-templates
+      '(("t" "(W) New task" entry (file "~/org/work/refile.org")
+         "* NEW %?\n  %i\n")
+        ("n" "(W) Note" entry (file "~/org/work/refile.org")
+         "* %?\n  %i\n")
+        ("T" "(H) New task" entry (file "~/org/personal/refile.org")
+         "* NEW %?\n  %i\n")
+        ("N" "(H) Note" entry (file "~/org/personal/refile.org")
+         "* %?\n  %i\n")))
+
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEW(n)"  "|" "DONE(d@/!)")
+              (sequence "WAITING(w@/!)" "MAYBE(h@/!)" "|" "CANCELLED(c@/!)"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO"      :foreground "green"  :weight bold)
+              ("NEW"       :foreground "red"    :weight bold)
+              ("DONE"      :foreground "gray"   :weight bold)
+              ("WAITING"   :foreground "orange" :weight bold)
+              ("MAYBE"     :foreground "yellow" :weight bold)
+              ("CANCELLED" :foreground "gray"   :weight bold))))
+
+;;
+;; htmlize doesn't work properly in batch mode
+;;
+;; Solarized light
+;; (custom-set-faces
+;;  '(default ((t (:foreground "#586e75" :background "#fdf6e3")))))
+;;
+;; Solarized dark
+ (custom-set-faces
+   '(default ((t (:foreground "#93a1a1" :background "#002b36")))))
+
+(require 'htmlize)
+
+;; Use this to play with css styles
+;; (setq org-export-htmlize-output-type 'css)
+
+(org-agenda-list nil nil 1)
+(org-agenda-write  "/tmp/agenda.html")
+
+(provide 'generate-agenda)
