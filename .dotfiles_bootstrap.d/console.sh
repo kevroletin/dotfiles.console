95226599554f0321c0ab9bae6fd6159a54918a96
Vasiliy Kevroletin <kevroletin@gmail.com>
2016-12-13 18:04:55 +1000

    Small fix

--- .dotfiles_bootstrap.d/console.sh
+++ .dotfiles_bootstrap.d/console.sh
@@ -3,7 +3,7 @@
 PACKAGES="vcsh mr zsh ranger ispell lynx vim"
 
 echo "echo === Changing default shell to zsh ===" >> $RESULT_SCRIPT_FILE
-echo "sudo chsh `whoami` `which zsh`" >> $RESULT_SCRIPT_FILE
+echo "sudo chsh `whoami` -s `which zsh`" >> $RESULT_SCRIPT_FILE
 
 while true; do
     read -p "Do you wish to install console version of emacs? " yn
